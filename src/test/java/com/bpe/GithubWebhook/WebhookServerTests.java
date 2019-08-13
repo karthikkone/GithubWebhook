2a5a1eebb8b84aa984ba740345235285cf97c628
karthikkone <iAgilePro@outlook.com>
2019-08-13 15:05:54 +0530

    added tests

---
+++ src/test/java/com/bpe/GithubWebhook/WebhookServerTests.java
@@ -0,0 +1,34 @@
+package com.bpe.GithubWebhook;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class WebhookServerTests {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	@Autowired
+	WebhookServer controller;
+	
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+	
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/", String.class)).contains("Github Wrapper Demo!");
+	}
+}
