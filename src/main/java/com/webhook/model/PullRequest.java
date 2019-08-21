f79eea69e66eaad2494c0794d64c8123bce0357e
Mohit Sharma <mohit@Mohits-MacBook-Air.local>
2019-08-21 18:58:34 +0530

    add model for PullRequest payload

--- src/main/java/com/webhook/model/PullRequest.java
+++ src/main/java/com/webhook/model/PullRequest.java
@@ -1,28 +1,17 @@
 package com.webhook.model;
 
-public class PullRequest {
-	
-	@Override
-	public String toString() {
-		return "PullRequest [number=" + number + ", state=" + state + ", title=" + title + ", body=" + body
-				+ ", _links=" + _links + ", draft=" + draft + ", id=" + id + "]";
-	}
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@ToString(includeFieldNames = true)
+public class PullRequest {
 
 	private String number; //pull request number
 	private String state; //pull state opened, created, closed etc.
 	private String title;
 	private String body;
 	private Links _links;
-	public Links get_links() {
-		return _links;
-	}
-
-	public void set_links(Links _links) {
-		this._links = _links;
-	}
-
-
 	private boolean draft;
 	private String id;
 	
@@ -33,89 +22,26 @@ public class PullRequest {
 		// TODO Auto-generated constructor stub
 	}
 
-
-	class Links {
+	@Data
+    @ToString(includeFieldNames = true)
+	static class Links {
 		Commits commits;
 		
 		public Links() {
 			super();
 		}
-
-		public Commits getCommits() {
-			return commits;
-		}
-
-		public void setCommits(Commits commits) {
-			this.commits = commits;
-		}
 	}
-	
-	class Commits {
+
+	@Data
+    @ToString(includeFieldNames = true)
+	static class Commits {
 		String href;
 		
 		public Commits() {
 			super();
 		}
-		public String getHref() {
-			return href;
-		}
-
-		public void setHref(String href) {
-			this.href = href;
-		}
 		
 	}
 
-	public String getNumber() {
-		return number;
-	}
 
-	public void setNumber(String number) {
-		this.number = number;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getBody() {
-		return body;
-	}
-
-	public void setBody(String body) {
-		this.body = body;
-	}
-
-	public boolean isDraft() {
-		return draft;
-	}
-
-	public void setDraft(boolean draft) {
-		this.draft = draft;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	
-	public String getCommitsLink() {
-		return this._links.commits.href;
-	}
 }
--- src/main/java/com/webhook/model/PullRequestPayload.java
+++ src/main/java/com/webhook/model/PullRequestPayload.java
@@ -1,5 +1,10 @@
 package com.webhook.model;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(includeFieldNames = true)
 public class PullRequestPayload {
 	
 	private String action; //pull event type opened, created etc;
@@ -9,35 +14,4 @@ public class PullRequestPayload {
 	public PullRequestPayload() {
 		super();
 	}
-
-	public String getAction() {
-		return action;
-	}
-
-	public void setAction(String action) {
-		this.action = action;
-	}
-
-	public Integer getNumber() {
-		return number;
-	}
-
-	public void setNumber(Integer number) {
-		this.number = number;
-	}
-
-	public PullRequest getPull_request() {
-		return pull_request;
-	}
-
-	public void setPull_request(PullRequest pull_request) {
-		this.pull_request = pull_request;
-	}
-
-	@Override
-	public String toString() {
-		return "PullRequestPayload [action=" + action + ", number=" + number + ", pull_request=" + pull_request + "]";
-	}
-
-	
 }
