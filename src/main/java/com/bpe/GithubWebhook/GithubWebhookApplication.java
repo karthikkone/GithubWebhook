53e985031b4879c68ad525b1a4508f11f57cf027
karthikkone <iAgilePro@outlook.com>
2019-08-30 19:06:12 +0530

    added Bean RestTemplate

--- src/main/java/com/bpe/GithubWebhook/GithubWebhookApplication.java
+++ src/main/java/com/bpe/GithubWebhook/GithubWebhookApplication.java
@@ -2,9 +2,11 @@ package com.bpe.GithubWebhook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.Executor;
 
@@ -25,6 +27,11 @@ public class GithubWebhookApplication {
 		executor.setThreadNamePrefix("GitLookUp-");
 		executor.initialize();
 		return executor;
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
