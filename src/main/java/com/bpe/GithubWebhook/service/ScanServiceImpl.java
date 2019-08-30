cf30d41783a8240d86ee7f488255841f6cf69638
karthikkone <iAgilePro@outlook.com>
2019-08-30 19:00:03 +0530

    read workspace pmd_path from env

--- src/main/java/com/bpe/GithubWebhook/service/ScanServiceImpl.java
+++ src/main/java/com/bpe/GithubWebhook/service/ScanServiceImpl.java
@@ -1,13 +1,16 @@
 package com.bpe.GithubWebhook.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +21,19 @@ import com.webhook.model.PullRequest;
 
 @Service
 public class ScanServiceImpl implements ScanService {
-
+	
+	@Value("${pmd.path}")
+	private String pmdPath;
+	
+	@Value("{$workspace.path:/tmp}")
+	private String workspace;
+	
+	@Value("{$pmd.rulespath}")
+	private String rulesPath;
+	
+	@Value("{$pmd.output:text}")
+	private String pmdOutputFormat;
+	
 	@Autowired
 	private GitLookUpService gitLookupService;
 	
@@ -32,6 +47,7 @@ public class ScanServiceImpl implements ScanService {
 			try {
 				List<PullCommit> pullCommits = gitLookupService.getPullCommits(pr).get();
 				
+				System.out.println("PULLCOMMITS : "+pullCommits);
 				if (!pullCommits.isEmpty()) {
 					for (PullCommit pc : pullCommits) {
 						List<Commit> commits = gitLookupService.getCommit(pc).get();
@@ -43,17 +59,35 @@ public class ScanServiceImpl implements ScanService {
 						}
 					}
 				}
-				
+				System.out.println("Commits of Pull request: "+commitsOfPR);
 				if (!commitsOfPR.isEmpty()) {
+					//create a directory for current PullRequest
+					new File(""+workspace+"/"+pr.getNumber());
+					
 					for (Commit c : commitsOfPR) {
 						List<Commit.CommitFile> commitedFiles = c.getFiles();
-						//TODO scan files
+						//create a directory for commit
+						File filesOfCommit = new File(""+workspace+"/"+pr.getNumber()+"/"+c.getSha());
+						
+						//fetch and save changed files to commits directory
+						for (Commit.CommitFile f: commitedFiles) {
+							FileUtils.copyURLToFile(new URL(f.getRaw_url()), filesOfCommit);
+						}
 					}
+					
+					//execute pmd
+					
 				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
